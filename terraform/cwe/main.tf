98d09ee2d8254cafc8e71589f52667e8a6179b9d
Richard Julian <richard@rjulian.net>
2020-05-17 12:03:18 -0700

    Fix rds camelcase

--- terraform/cwe/main.tf
+++ terraform/cwe/main.tf
@@ -1,6 +1,6 @@
 module "cwe" {
   source      = "git::https://github.com/cloudmitigator/reflex-engine.git//modules/cwe_lambda?ref=v0.6.0"
-  name        = "RDSPublicSnapshot"
+  name        = "RdsPublicSnapshot"
   description = "Rule that alerts when a RDS snapshot is set to be public"
 
   event_pattern = <<PATTERN
