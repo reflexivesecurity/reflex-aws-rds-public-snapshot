6c8f1fda3e401eed197a7d61a9314470af856091
Richard Julian <richard@rjulian.net>
2020-05-17 12:02:55 -0700

    Fix source code dir and camelcase

--- terraform/sqs_lambda/sqs_lambda.tf
+++ terraform/sqs_lambda/sqs_lambda.tf
@@ -2,8 +2,8 @@ module "sqs_lambda" {
   source = "git::https://github.com/cloudmitigator/reflex-engine.git//modules/sqs_lambda?ref=v0.6.0"
   cloudwatch_event_rule_id  = var.cloudwatch_event_rule_id
   cloudwatch_event_rule_arn = var.cloudwatch_event_rule_arn
-  function_name   = "RDSPublicSnapshot"
-  source_code_dir = "${path.module}/source"
+  function_name   = "RdsPublicSnapshot"
+  source_code_dir = "${path.module}/../../source"
   handler         = "reflex_aws_rds_public_snapshot.lambda_handler"
   lambda_runtime  = "python3.7"
   environment_variable_map = {
@@ -13,10 +13,10 @@ module "sqs_lambda" {
 
 
 
-  queue_name    = "RDSPublicSnapshot"
+  queue_name    = "RdsPublicSnapshot"
   delay_seconds = 0
 
-  target_id = "RDSPublicSnapshot"
+  target_id = "RdsPublicSnapshot"
 
   sns_topic_arn  = var.sns_topic_arn
   sqs_kms_key_id = var.reflex_kms_key_id
